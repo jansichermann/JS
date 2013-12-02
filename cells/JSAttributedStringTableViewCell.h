44ace8725bbda396b09856e743193c609a20abf5
Jan Sichermann <jan@urbancompass.com>
2013-12-01 23:26:50 -0500

    multiline on attributed

--- cells/JSAttributedStringTableViewCell.h
+++ cells/JSAttributedStringTableViewCell.h
@@ -1,6 +1,16 @@
 #import "JSTableViewCell.h"
 
 
+@interface JSAttributedStringTableViewCellModel : NSObject
++ (instancetype)withText:(NSAttributedString *)text
+        topBottomPadding:(CGFloat)topBottomPadding;
++ (instancetype)withText:(NSAttributedString *)text;
++ (instancetype)withText:(NSAttributedString *)text
+                    icon:(UIImage *)icon;
++ (instancetype)withText:(NSAttributedString *)text
+                    icon:(UIImage *)icon
+        topBottomPadding:(CGFloat)topBottomPadding;
+@end
 
 /**
  Created by jan on 11/18/13. Copyright (c) 2013 Jan Sichermann. All rights reserved.
--- cells/JSAttributedStringTableViewCell.m
+++ cells/JSAttributedStringTableViewCell.m
@@ -1,22 +1,78 @@
 #import "JSAttributedStringTableViewCell.h"
+
+#include "JSMacros.h"
+
 #import "UIView+JS.h"
 
 
 
-@implementation JSAttributedStringTableViewCell
+@interface JSAttributedStringTableViewCellModel()
+@property (nonatomic)           NSAttributedString          *text;
+@property (nonatomic)           CGFloat                     topBottomPadding;
+@property (nonatomic)           UIImage                     *icon;
+@end
 
-+ (CGFloat)heightForModel:(NSAttributedString *)model
-              inTableView:(UITableView *)tableView {
-    NSParameterAssert([model isKindOfClass:[NSAttributedString class]]);
-    return [model boundingRectWithSize:CGSizeMake(tableView.width,
-                                                  INT32_MAX)
-                               options:0
-                               context:nil].size.height;
+
+
+@implementation JSAttributedStringTableViewCellModel
+
++ (instancetype)withText:(NSAttributedString *)text {
+    return [self withText:text topBottomPadding:0];
 }
 
-- (void)configureWithModel:(NSAttributedString *)model {
-    NSParameterAssert([model isKindOfClass:[NSAttributedString class]]);
-    self.textLabel.attributedText = model;
++ (instancetype)withText:(NSAttributedString *)text icon:(UIImage *)icon {
+    return [self withText:text icon:icon topBottomPadding:0];
+}
+
++ (instancetype)withText:(NSAttributedString *)text
+        topBottomPadding:(CGFloat)topBottomPadding {
+    return [self withText:text icon:nil topBottomPadding:topBottomPadding];
+}
+
++ (instancetype)withText:(NSAttributedString *)text
+                    icon:(UIImage *)icon
+        topBottomPadding:(CGFloat)topBottomPadding {
+    JSAttributedStringTableViewCellModel *m = [[JSAttributedStringTableViewCellModel alloc] init];
+    m.text = text;
+    m.icon = icon;
+    m.topBottomPadding = topBottomPadding;
+    return m;
+}
+
+
+
+@end
+
+
+@implementation JSAttributedStringTableViewCell
+
++ (CGFloat)heightForModel:(JSAttributedStringTableViewCellModel *)model
+              inTableView:(UITableView *)tableView {
+    NSParameterAssert([model isKindOfClass:[JSAttributedStringTableViewCellModel class]]);
+    NSParameterAssert([model.text isKindOfClass:[NSAttributedString class]]);
+    return [model.text boundingRectWithSize:CGSizeMake(tableView.width,
+                                                       CGFLOAT_MAX)
+                                    options:(NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading)
+                                    context:nil].size.height + 2 * model.topBottomPadding;
+}
+
+- (id)initWithStyle:(UITableViewCellStyle)style
+    reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style
+                reuseIdentifier:reuseIdentifier];
+    if (!self) {
+        return nil;
+    }
+    self.textLabel.numberOfLines = INT16_MAX;
+    UILabel *textLabel = self.textLabel;
+    UIImageView *imageView = self.imageView;
+    self.configureBlock = ^(id model) {
+        JSAttributedStringTableViewCellModel *cellModel = (JSAttributedStringTableViewCellModel *)model;
+        textLabel.attributedText = cellModel.text;
+        imageView.image = cellModel.icon;
+        [textLabel sizeToFit];
+    };
+    return self;
 }
 
 @end
