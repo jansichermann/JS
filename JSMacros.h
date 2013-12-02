e0f955db16dc82fb768797d354a2448c67fe06a7
Jan Sichermann <jan@urbancompass.com>
2013-12-01 23:19:56 -0500

    assert which can be passed in a weak refernce to self

--- JSMacros.h
+++ JSMacros.h
@@ -4,3 +4,16 @@
 #define WEAK(_obj) \
 __weak __typeof__(_obj) weak_ ## _obj = _obj
 
+#define JSLog(s, ...) NSLog((@"%@: " s), [self class], ##__VA_ARGS__);
+
+
+#define JSAssert(weak_self, condition, desc, ...)	\
+do {				\
+__PRAGMA_PUSH_NO_EXTRA_ARG_WARNINGS \
+if (!(condition)) {		\
+[[NSAssertionHandler currentHandler] handleFailureInMethod:_cmd \
+object:weak_self file:[NSString stringWithUTF8String:__FILE__] \
+lineNumber:__LINE__ description:(desc), ##__VA_ARGS__]; \
+}				\
+__PRAGMA_POP_NO_EXTRA_ARG_WARNINGS \
+} while(0)
