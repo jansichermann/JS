1a93f577b1beb3ad6bbbcbcaa4c08da0950baeb4
Jan Sichermann <jan@urbancompass.com>
2013-12-09 23:36:54 -0500

    textfield

---
+++ views/JSTextField.h
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+
+
+/**
+ Created by jan on 12/2/13. Copyright (c) 2013 Jan Sichermann. All rights reserved.
+ */
+
+
+typedef void(^JSTextFieldDelegateBlock)(UITextField *tf);
+@interface JSTextField : UITextField
+@property (nonatomic, copy)         JSTextFieldDelegateBlock        onValueChangeBlock;
+@property (nonatomic, copy)         JSTextFieldDelegateBlock        onDidEndEditingBlock;
+@property (nonatomic, copy)         JSTextFieldDelegateBlock        onEditingDidEndOnExitBlock;
+@property (nonatomic, copy)         JSTextFieldDelegateBlock        onDidBeginEditing;
+@end
---
+++ views/JSTextField.m
@@ -0,0 +1,49 @@
+#import "JSTextField.h"
+
+@interface JSTextField()
+@end
+
+@implementation JSTextField
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    if (!self) {
+        return nil;
+    }
+    [self addTarget:self
+             action:@selector(textFieldValueChanged:)
+   forControlEvents:UIControlEventEditingChanged];
+    [self addTarget:self
+             action:@selector(textFieldDidBeginEditing:) forControlEvents:UIControlEventEditingDidBegin];
+    [self addTarget:self
+             action:@selector(textFieldEditingDidEndOnExit:)
+   forControlEvents:UIControlEventEditingDidEndOnExit];
+    return self;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)tf {
+    if (self.onDidEndEditingBlock) {
+        self.onDidEndEditingBlock(tf);
+    }
+}
+
+- (void)textFieldEditingDidEndOnExit:(UITextField *)tf {
+    if (self.onEditingDidEndOnExitBlock) {
+        self.onEditingDidEndOnExitBlock(tf);
+    }
+}
+
+- (void)textFieldValueChanged:(UITextField *)tf {
+    if (self.onValueChangeBlock) {
+        self.onValueChangeBlock(tf);
+    }
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)tf {
+    if (self.onDidBeginEditing) {
+        self.onDidBeginEditing(tf);
+    }
+}
+
+@end
