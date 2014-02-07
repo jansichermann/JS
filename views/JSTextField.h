d48ac5f9d6a1028baf8033b0facb8bf49f397262
Jan Sichermann <jansichermann@gmail.com>
2014-02-07 00:54:27 -0500

    -n fix block

--- views/JSTextField.h
+++ views/JSTextField.h
@@ -1,16 +1,20 @@
 #import <UIKit/UIKit.h>
 
 
+@class JSTextField;
+typedef void(^JSTextFieldDelegateBlock)(JSTextField *tf);
+
+
 
 /**
  Created by jan on 12/2/13. Copyright (c) 2013 Jan Sichermann. All rights reserved.
  */
 
 
-typedef void(^JSTextFieldDelegateBlock)(UITextField *tf);
+
 @interface JSTextField : UITextField
-@property (nonatomic, copy)         JSTextFieldDelegateBlock        onValueChangeBlock;
-@property (nonatomic, copy)         JSTextFieldDelegateBlock        onDidEndEditingBlock;
-@property (nonatomic, copy)         JSTextFieldDelegateBlock        onEditingDidEndOnExitBlock;
-@property (nonatomic, copy)         JSTextFieldDelegateBlock        onDidBeginEditing;
+@property (nonatomic, copy) JSTextFieldDelegateBlock        onValueChangeBlock;
+@property (nonatomic, copy) JSTextFieldDelegateBlock        onDidEndEditingBlock;
+@property (nonatomic, copy) JSTextFieldDelegateBlock        onEditingDidEndOnExitBlock;
+@property (nonatomic, copy) JSTextFieldDelegateBlock        onDidBeginEditing;
 @end
