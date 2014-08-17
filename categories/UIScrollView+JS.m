f287d2d41d47b7957b78a1be8752c6dbd836e0f6
Jan Sichermann <jansichermann@gmail.com>
2014-08-17 13:31:27 -0400

    weak reference

--- categories/UIScrollView+JS.m
+++ categories/UIScrollView+JS.m
@@ -8,12 +8,17 @@
     if (self.contentSize.height < self.height) {
         return;
     }
+    
+    __weak UIScrollView *weakSelf = self;
+    
+    CGFloat newHeight = self.contentSize.height - self.height;
+    
     [UIView animateWithDuration:0.3
-                     animations:^{
-                         self.contentOffset =
-                         CGPointMake(0,
-                                     self.contentSize.height - self.height);
-                     }
+                     animations:
+     ^{
+         UIScrollView *strongSelf = weakSelf;
+         strongSelf.contentOffset = CGPointMake(0, newHeight);
+     }
                      completion:completionBlock];
 }
 
