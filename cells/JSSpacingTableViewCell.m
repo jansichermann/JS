51b0e85a5c4e513e49d0247102d17ef5c9052fac
Jan Sichermann <jansichermann@gmail.com>
2014-09-03 22:43:54 -0400

    selectable spacingcell

--- cells/JSSpacingTableViewCell.m
+++ cells/JSSpacingTableViewCell.m
@@ -1,24 +1,8 @@
 #import "JSSpacingTableViewCell.h"
-#include "JSMacros.h"
 
 
 
 @implementation JSSpacingTableViewCell
-
-- (id)initWithStyle:(UITableViewCellStyle)style
-    reuseIdentifier:(NSString *)reuseIdentifier {
-    
-    self = [super initWithStyle:style
-                reuseIdentifier:reuseIdentifier];
-    
-    WEAK(self);
-    
-    self.configureBlock = ^(__unused id model) {
-        JSSpacingTableViewCell *strongSelf = weak_self;
-        strongSelf.selectionStyle = UITableViewCellSelectionStyleNone;
-    };
-    return self;
-}
 
 + (CGFloat)heightForModel:(NSNumber *)model
               inTableView:(__unused UITableView *)tableView {
